2eee5217ab6e11c5b02814da496597c022c42d6b
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-06-25 15:55:07 +0530

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -4,28 +4,22 @@ public class Employee
 {
 	public static void main(String[] args) 
 	{
-                System.out.println("Welcome to Employee Wage Computation Program");
-		
 		// create instance of Random class
         Random rand = new Random();
    
         // Generate random integers in range 0 to 2 
         int attendance = rand.nextInt(3);
         
-        final int FullTime_working_Hours=8;
-        final int PartTime_working_Hours=4;
+        final int Total_FullTime_working_Hours=160;
+        final int Total_PartTime_working_Hours=80;
         final int Working_Days=20;
         final int WAGE_PER_HOUR = 20;
-        
-
-    	final int Total_FullTime_Hours=160;
-    	final int Total_PartTime_Hours=80;
     	
     	final int Total_Working_Hours=100;
     	
     	int MonthlyWages=0;
     	
-    	if(Working_Days==20 && Total_Working_Hours==100)
+    	if(Working_Days==20 || Total_FullTime_working_Hours==100 || Total_PartTime_working_Hours==100)
     	{		
     		switch(attendance)
     		{
@@ -34,16 +28,17 @@ public class Employee
     			
     		case 1:
     			System.out.println("FullTime Employee");
-    			MonthlyWages=FullTime_working_Hours*Working_Days*WAGE_PER_HOUR;
+    			MonthlyWages=Total_FullTime_working_Hours*WAGE_PER_HOUR;
     			System.out.println("Monthly Wage is:"+MonthlyWages );
     			
     		case 2:
     			System.out.println("PartTime Employee");
-    			MonthlyWages=PartTime_working_Hours*Working_Days*WAGE_PER_HOUR;;
+    			MonthlyWages=Total_PartTime_working_Hours*WAGE_PER_HOUR;;
     			System.out.println("Mothly Wage is:"+MonthlyWages );
     			
     		}
-    	}	
+    	}
+		
 		
 	}
 
