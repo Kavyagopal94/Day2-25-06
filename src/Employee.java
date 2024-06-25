d6a7a5af944c0e192108bea2e250ade67c4ed310
Kavya <kavyagopal94@gmail.com>
2024-06-25 12:40:56 +0530

    partTime Wages

--- src/Employee.java
+++ src/Employee.java
@@ -9,23 +9,27 @@ public class Employee
 		// create instance of Random class
         Random rand = new Random();
    
-        // Generate random integers in range 0 to 2 and 0 to 8
+        // Generate random integers in range 0 to 2 and 0 to 8 and 0 to 4
         int attendance = rand.nextInt(3);
         int Worked_Hours=rand.nextInt(9);
+        int PartTime_worked_Hours=rand.nextInt(5);
         
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOUR = 8;
+        final int PART_TIME_HOURS = 4;
 		
 		if(attendance==1)
 		{
-			System.out.println("Employee is present");
+			System.out.println("FullTime Employee");
 			int dailyWage=WAGE_PER_HOUR*Worked_Hours;
 			System.out.println("Daily Wage is:"+dailyWage );
 			
 		}
-		else
+		else if(attendance==2)
 		{
-			System.out.println("Employee is absent");
+			System.out.println("PartTime Employee");
+			int dailyWage=WAGE_PER_HOUR*PartTime_worked_Hours;
+			System.out.println("Daily Wage is:"+dailyWage );
 		}
 	}
 
