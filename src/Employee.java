fadbbf480780a5275b786475ba176f0a70dd5554
Kavya <kavyagopal94@gmail.com>
2024-06-25 12:26:00 +0530

    DailyWage Calculated

--- src/Employee.java
+++ src/Employee.java
@@ -9,12 +9,19 @@ public class Employee
 		// create instance of Random class
         Random rand = new Random();
    
-        // Generate random integers in range 0 to 1
-        int attendance = rand.nextInt(2);
+        // Generate random integers in range 0 to 2 and 0 to 8
+        int attendance = rand.nextInt(3);
+        int Worked_Hours=rand.nextInt(9);
+        
+        final int WAGE_PER_HOUR = 20;
+        final int FULL_DAY_HOUR = 8;
 		
 		if(attendance==1)
 		{
 			System.out.println("Employee is present");
+			int dailyWage=WAGE_PER_HOUR*Worked_Hours;
+			System.out.println("Daily Wage is:"+dailyWage );
+			
 		}
 		else
 		{
