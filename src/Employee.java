3aa6200535b67b0be89a06713fb46aadb940d50b
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-06-25 14:46:25 +0530

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -11,15 +11,15 @@ public class Employee
    
         // Generate random integers in range 0 to 2 and 0 to 8
         int attendance = rand.nextInt(3);
-        int Worked_Hours=rand.nextInt(9);
         
+        final int Working_Hours=8;
         final int WAGE_PER_HOUR = 20;
-        final int FULL_DAY_HOUR = 8;
+        
 		
 		if(attendance==1)
 		{
 			System.out.println("Employee is present");
-			int dailyWage=WAGE_PER_HOUR*Worked_Hours;
+			int dailyWage=WAGE_PER_HOUR*Working_Hours;
 			System.out.println("Daily Wage is:"+dailyWage );
 			
 		}
