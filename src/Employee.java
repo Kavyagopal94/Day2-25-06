c2511856a53faea5a540875a9f01a2875161df13
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-06-25 15:32:12 +0530

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -4,27 +4,28 @@ public class Employee
 {
 	public static void main(String[] args) 
 	{
-	
-		System.out.println("Welcome to Employee Wage Computation Program");
+                System.out.println("Welcome to Employee Wage Computation Program");
 		
 		// create instance of Random class
         Random rand = new Random();
    
-        // Generate random integers in range 0 to 2 and 0 to 8 and 0 to 4 and 0 to 19
+        // Generate random integers in range 0 to 2 
         int attendance = rand.nextInt(3);
-        int FullTime_worked_Hours=rand.nextInt(9);
-        int PartTime_worked_Hours=rand.nextInt(5);
-        int Worked_Days=rand.nextInt(21);
         
+        final int FullTime_working_Hours=8;
+        final int PartTime_working_Hours=4;
+        final int Working_Days=20;
         final int WAGE_PER_HOUR = 20;
         
-    	int MonthlyWages=0;
-    	int dailyWage=0;
-    	int Total_Worked_hoursF=Worked_Days*FullTime_worked_Hours;
-    	int Total_Worked_hoursP=Worked_Days*PartTime_worked_Hours;
+
+    	final int Total_FullTime_Hours=160;
+    	final int Total_PartTime_Hours=80;
     	
-		
-    	if(Worked_Days==20 && (Total_Worked_hoursF==100 || Total_Worked_hoursF==100))
+    	final int Total_Working_Hours=100;
+    	
+    	int MonthlyWages=0;
+    	
+    	if(Working_Days==20 && Total_Working_Hours==100)
     	{		
     		switch(attendance)
     		{
@@ -33,19 +34,16 @@ public class Employee
     			
     		case 1:
     			System.out.println("FullTime Employee");
-    			dailyWage=WAGE_PER_HOUR*FullTime_worked_Hours;
-    			MonthlyWages=dailyWage*Worked_Days;
+    			MonthlyWages=FullTime_working_Hours*Working_Days*WAGE_PER_HOUR;
     			System.out.println("Monthly Wage is:"+MonthlyWages );
     			
     		case 2:
     			System.out.println("PartTime Employee");
-    			dailyWage=WAGE_PER_HOUR*PartTime_worked_Hours;
-    			MonthlyWages=dailyWage*Worked_Days;
+    			MonthlyWages=PartTime_working_Hours*Working_Days*WAGE_PER_HOUR;;
     			System.out.println("Mothly Wage is:"+MonthlyWages );
     			
     		}
-    	}
-		
+    	}	
 		
 	}
 
