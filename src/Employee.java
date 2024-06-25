2d6d6aa4c51a8030490f0b21a70d7306717dae3f
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-06-25 14:50:43 +0530

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -11,13 +11,11 @@ public class Employee
    
         // Generate random integers in range 0 to 2 and 0 to 8 and 0 to 4 and 0 to 19
         int attendance = rand.nextInt(3);
-        int FullTime_worked_Hours=rand.nextInt(9);
-        int PartTime_worked_Hours=rand.nextInt(5);
-        int Worked_Days=rand.nextInt(21);
-        
+       
+        final int Working_Days=20 ;
         final int WAGE_PER_HOUR = 20;
         final int FULL_TIME_HOUR = 8;
-        final int PART_TIME_HOURS = 4;
+        final int PART_TIME_HOUR = 4;
         
     	int MonthlyWages=0;
     	int dailyWage=0;
@@ -29,14 +27,14 @@ public class Employee
 			System.out.println("Employee Absent");
 		case 1:
 			System.out.println("FullTime Employee");
-			dailyWage=WAGE_PER_HOUR*FullTime_worked_Hours;
-			MonthlyWages=dailyWage*Worked_Days;
+			dailyWage=WAGE_PER_HOUR*FUll_TIME_HOUR;
+			MonthlyWages=dailyWage*Working_Days;
 			System.out.println("Monthly Wage is:"+MonthlyWages );
 			
 		case 2:
 			System.out.println("PartTime Employee");
-			dailyWage=WAGE_PER_HOUR*PartTime_worked_Hours;
-			MonthlyWages=dailyWage*Worked_Days;
+			dailyWage=WAGE_PER_HOUR*PART_TIME_HOUR;
+			MonthlyWages=dailyWage*Working_Days;
 			System.out.println("Mothly Wage is:"+MonthlyWages );
 			
 		}
