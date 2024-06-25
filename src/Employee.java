4eaadc05b0c94324b5337d907e91e3150f810ef0
Kavya <kavyagopal94@gmail.com>
2024-06-25 12:03:37 +0530

    useCase1

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,25 @@
+import java.util.Random;
+
 public class Employee
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		// create instance of Random class
+        Random rand = new Random();
+   
+        // Generate random integers in range 0 to 1
+        int attendance = rand.nextInt(2);
+		
+		if(attendance==1)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
+
 }
