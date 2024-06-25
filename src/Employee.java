7f95de2816862bcc1bcd9f7684eaaf317d4bb0e4
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-06-25 14:57:14 +0530

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -11,12 +11,11 @@ public class Employee
    
         // Generate random integers in range 0 to 2 and 0 to 8 and 0 to 4
         int attendance = rand.nextInt(3);
-        int Worked_Hours=rand.nextInt(9);
-        int PartTime_worked_Hours=rand.nextInt(5);
-        
+		
+	final int Working_Days=20 ;
         final int WAGE_PER_HOUR = 20;
         final int FULL_DAY_HOUR = 8;
-        final int PART_TIME_HOURS = 4;
+        final int PART_TIME_HOUR = 4;
 	
 		switch(attendance)
 		{
@@ -24,12 +23,12 @@ public class Employee
 			System.out.println("Employee Absent");
 		case 1:
 			System.out.println("FullTime Employee");
-			int dailyWage=WAGE_PER_HOUR*Worked_Hours;
+			int dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
 			System.out.println("Daily Wage is:"+dailyWage );
 			
 		case 2:
 			System.out.println("PartTime Employee");
-			int dailyWage1=WAGE_PER_HOUR*PartTime_worked_Hours;
+			int dailyWage1=WAGE_PER_HOUR*PART_TIME_HOUR;
 			System.out.println("Daily Wage is:"+dailyWage1 );
 			
 		}
